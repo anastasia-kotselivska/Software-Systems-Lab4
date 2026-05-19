6b2ec35190f0ec71eea29a4a41a5262818aa3d98
Anastasia <anastasiia.kotselivska.23@pnu.edu.ua>
2026-05-19 19:53:28 +0300

    Fix linting formatting errors

--- test_calculator.py
+++ test_calculator.py
@@ -7,11 +7,9 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
 
-
     def test_subtract(self):
         self.assertEqual(subtract(5, 2), 3)
 
 
 if __name__ == '__main__':
     unittest.main()
-
